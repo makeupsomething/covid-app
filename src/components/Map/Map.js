03142403d13b1fd90c90458b4226f8d69fc8498f
Daryl <darylcoleoffice@gmail.com>
2020-05-31 15:37:46 +0900

    Update map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,6 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import styled from 'styled-components';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, GeoJSON } from 'react-leaflet';
+
+import newYorkGeoJson from './data/geojson/new_york.json';
+
+const MAPBOX_ACCESS_TOKEN = `${process.env.REACT_APP_MAP_BOX_KEY}`;
+const MAPBOX_STYLE_ID = `${process.env.REACT_APP_MAPBOX_STYLE_ID}`;
+const MAPBOX_USERNAME = `${process.env.REACT_APP_MAPBOX_USERNAME}`;
+const defaultMap =
+	'https://api.mapbox.com/styles/v1/' +
+	MAPBOX_USERNAME +
+	'/' +
+	MAPBOX_STYLE_ID +
+	'/tiles/256/{z}/{x}/{y}?access_token=' +
+	MAPBOX_ACCESS_TOKEN;
 
 const MapWrapper = styled.div`
 	height: 600px;
@@ -14,17 +27,71 @@ const MapWrapper = styled.div`
 
 const DefaultMap = () => {
 	const [position, setPosition] = useState({
-		lat: 51.505,
-		lng: -0.09,
+		lng: -73.962855463380421,
+		lat: 40.713571109780113,
 	});
-	const [zoom, setZoom] = useState(13);
+	const [zoom, setZoom] = useState(10);
+	const mapRef = useRef(null);
+
+	useEffect(() => {
+		console.log(mapRef);
+	}, [mapRef]);
+
+	const highlightFeature = (e) => {
+		const layer = e.target;
+
+		layer.setStyle({
+			weight: 5,
+			dashArray: '',
+			fillColor: 'red',
+			fillOpacity: 0.2,
+		});
+	};
+
+	const resetHighlight = (e) => {
+		const layer = e.target;
+
+		layer.setStyle({
+			color: '#c0464c',
+			weight: 2,
+			fillColor: 'transparent',
+			fillOpacity: 1,
+		});
+	};
+
+	const zoomToFeature = (e) => {
+		mapRef.current.leafletElement.fitBounds(e.target.getBounds());
+	};
+
+	const onEachFeature = (feature, layer) => {
+		layer.on({
+			mouseover: highlightFeature,
+			mouseout: resetHighlight,
+			click: zoomToFeature,
+		});
+	};
 
 	return (
 		<MapWrapper>
-			<Map id="map" center={[position.lat, position.lng]} zoom={zoom}>
+			<Map
+				id="map"
+				ref={mapRef}
+				center={[position.lat, position.lng]}
+				zoom={zoom}
+			>
+				<GeoJSON
+					data={newYorkGeoJson}
+					style={() => ({
+						color: '#c0464c',
+						weight: 2,
+						fillColor: 'transparent',
+						fillOpacity: 1,
+					})}
+					onEachFeature={onEachFeature}
+				/>
 				<TileLayer
 					attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+					url={defaultMap}
 				/>
 			</Map>
 		</MapWrapper>
